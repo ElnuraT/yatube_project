879b3efe2b56d7d1af1568cb7ba8c5b5ceef62b6
Эльнура Тынаева <tyn_e@mail.ru>
2022-11-30 03:47:21 +0500

    render connection

---
+++ yatube/posts/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Главная страница
+def index(request):
+    return render(request, 'posts/index.html')
+
+
+# Страница конкретной группы
+def group_posts(request, slug):
+    return HttpResponse(f'Страница группы {slug}')
